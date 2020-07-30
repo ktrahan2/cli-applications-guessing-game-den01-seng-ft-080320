4a5318ea78bf5aa8b007af3ccf2d858f0f752d2d
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-30 18:45:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,6 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   roll = rand(6) + 1
   guess = gets.chomp.to_i
-  binding.pry
   if guess == roll 
     print "You guessed the correct number!"
   elsif guess != roll
